033b1ca8a21d6dcd4acc601b5c4003943e5fd3ec
KokiNishimura <89366027+nisiko36@users.noreply.github.com>
2023-08-20 12:03:11 +0000

    [finish]4-10

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数にからのModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     @list = List.new(list_params)
     if @list.save
@@ -19,25 +19,25 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    
+
   end
 
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
-  
+
   private
   # ストロングパラメータ
   def list_params
